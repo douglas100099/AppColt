c3a92d80da20ec0c6f8274369ed83d7fc2a63208
Yuri Perro <yuribaumgartner@ice.ufjf.br>
2020-09-13 01:47:52 -0300

    Commit inicial

---
+++ GrabCab_Code/GrabCabWeb_code/admin/src/config/keys.js
@@ -0,0 +1,14 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyDJty-EsGLTBg1esYp0FDWV1bARFWE5DHQ",
+    authDomain: "app-bolt-e3395.firebaseapp.com",
+    databaseURL: "https://app-bolt-e3395.firebaseio.com",
+    projectId: "app-bolt-e3395",
+    storageBucket: "app-bolt-e3395.appspot.com",
+    messagingSenderId: "481937108307",
+    appId: "1:481937108307:web:4669f57ce4bdce2faac5a6",
+    measurementId: "G-VF3C9V8B4V"
+};
+
+export const google_map_key = 'AIzaSyBAlxYhT0LIXiKtSY1TnOr-iAnEKFgj3aE';
+
+export const isLive = true;

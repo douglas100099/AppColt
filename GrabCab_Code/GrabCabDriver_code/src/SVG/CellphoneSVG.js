bf8ac635cd863dde1aa18461989a13d11f536b0a
Your Name <you@example.com>
2020-11-09 21:45:27 -0300

    Arrumando marker angle

--- GrabCab_Code/GrabCabDriver_code/src/SVG/CellphoneSVG.js
+++ GrabCab_Code/GrabCabDriver_code/src/SVG/CellphoneSVG.js
@@ -1,16 +1,32 @@
 import * as React from "react"
-import Svg, { Circle, Path } from "react-native-svg"
+import Svg, { G, Circle, Path, Defs, ClipPath } from "react-native-svg"
 
 function CellphoneSVG(props) {
   return (
     <Svg width={40} height={40} viewBox="0 0 40 40" fill="none" {...props}>
-      <Circle cx={20} cy={20} r={20} fill="#fff" />
-      <Path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M20 3.5c9.113 0 16.5 7.387 16.5 16.5S29.113 36.5 20 36.5 3.5 29.113 3.5 20 10.887 3.5 20 3.5zm-4.502 22.595a1 1 0 01-1.398-.918v-3.101a.5.5 0 01.427-.495l9.287-1.38-9.287-1.382a.5.5 0 01-.427-.494v-3.102a1 1 0 011.398-.917L28.04 19.74a.5.5 0 010 .918l-12.543 5.436z"
-        fill="#1152FD"
-      />
+      <G clipPath="url(#prefix__clip0)">
+        <Circle
+          cx={20}
+          cy={20}
+          r={20}
+          transform="rotate(-90 20 20)"
+          fill="#fff"
+        />
+        <Path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M3.5 20c0-9.113 7.387-16.5 16.5-16.5S36.5 10.887 36.5 20 29.113 36.5 20 36.5 3.5 29.113 3.5 20zm22.894 4.602A1 1 0 0125.477 26h-3.101a.5.5 0 01-.495-.427l-1.38-9.287-1.382 9.287a.5.5 0 01-.494.427h-3.102a1 1 0 01-.917-1.398L20.04 12.06a.5.5 0 01.918 0l5.435 12.543z"
+          fill="#1152FD"
+        />
+      </G>
+      <Defs>
+        <ClipPath id="prefix__clip0">
+          <Path
+            d="M20 0C8.954 0 0 8.954 0 20s8.954 20 20 20 20-8.954 20-20S31.046 0 20 0z"
+            fill="#fff"
+          />
+        </ClipPath>
+      </Defs>
     </Svg>
   )
 }

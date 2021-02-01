08fef4ed6cbe3dfa8dc5fe69d8a9cb1c9b44db22
Yuri Baumgartner <yuribaumgartner2@outlook.com>
2021-02-01 20:34:30 -0300

    Primeiras mudanças pra teste

--- Colt_code/ColtPassageiro_code/src/screens/IntroScreen.js
+++ Colt_code/ColtPassageiro_code/src/screens/IntroScreen.js
@@ -252,7 +252,7 @@ export default class IntroScreen extends Component {
     }*/
 
     onPressLoginEmail = async () => {
-        if (this.state.dontCreateAccount == true) {
+        if (false) {
             Alert.alert(
                 'Ops!',
                 'No momento, atendemos a cidade de Valença-RJ. Certifique-se de criar sua conta nessa região!',
@@ -265,7 +265,7 @@ export default class IntroScreen extends Component {
                 { cancelable: true },
             );
         }
-        else if (this.state.dontCreateAccount == null) {
+        else if (false) {
             Alert.alert(
                 'Só mais um instante!',
                 'Estamos preparando o app pra você!',

308286f7a5e28d2bc2c00cf355fdb9678508a8ae
Yuri Perro <yuribaumgartner@ice.ufjf.br>
2021-02-14 12:21:19 -0300

    Adicionando bottom sheet a component

---
+++ Colt_code/ColtPassageiro_code/src/components/BottomSheetComponent.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import {
+    View,
+    TouchableOpacity,
+    Text,
+    StyleSheet,
+    Dimensions,
+
+} from 'react-native';
+import { Pulse, Wander } from 'react-native-animated-spinkit'
+import LocationDrop from '../../assets/svg/LocationDrop';
+import LocationUser from '../../assets/svg/LocationUser';
+import LocationWaypoint from '../../assets/svg/LocationWaypoint';
+import ColtEconomicoCar from '../../assets/svg/ColtEconomicoCar';
+import ColtConfortCar from '../../assets/svg/ColtConfortCar';
+import { colors } from '../common/theme';
+var { width, height } = Dimensions.get('window');
+
+
+const COLT_ECONOMICO = "Colt econômico"
+const COLT_CONFORT = "Colt confort"
+
+const BottomSheetComponent = ({ region, waypoint, payment, carType, customStyle }) => {
+    return (
+        <View
+            style={[styles.container, { ...customStyle }]}
+        >
+            <TouchableOpacity style={{ width: 65, alignSelf: 'center', height: 6, backgroundColor: colors.GREY1, borderRadius: 50 }} />
+            <View style={styles.viewQueueBooking}>
+                <Wander
+                    size={40}
+                    color={colors.DEEPBLUE}
+                />
+                <Text style={{ textAlign: 'center', paddingHorizontal: 5, fontFamily: 'Inter-ExtraBold', fontSize: 15, color: colors.DEEPBLUE }}> Estamos conectando você ao motorista. </Text>
+            </View>
+            <View>
+                <View style={{ marginTop: 15 }}>
+
+                    <View style={{ marginTop: 10, flexDirection: 'row', alignItems: 'center' }}>
+                        <LocationUser
+                            width={20}
+                            height={20}
+                        />
+                        <Text style={{ marginLeft: 5 }}>{region ? region.whereText.split(',')[0] + region.whereText.split(',')[1] : null}</Text>
+                    </View>
+
+                    {waypoint ?
+                        <View>
+                            <View style={{ marginTop: 10, flexDirection: 'row', alignItems: 'center' }}>
+                                <LocationWaypoint
+                                    width={20}
+                                    height={20}
+                                />
+                                <Text style={{ marginLeft: 5 }}>{waypoint.add}</Text>
+                            </View>
+                        </View>
+                        :
+                        null}
+
+                    <View style={{ marginTop: 10, flexDirection: 'row', alignItems: 'center' }}>
+                        <LocationDrop
+                            width={22}
+                            height={22}
+                        />
+                        <Text style={{ marginLeft: 5 }}>{region ? region.droptext.split(',')[0] + region.droptext.split(',')[1] : null}</Text>
+                    </View>
+
+
+                    {
+                        payment ?
+                            payment.payment_mode === 'Dinheiro' ?
+                                <View style={{ alignSelf: 'center', flexDirection: 'column', alignItems: 'center' }}>
+                                    <Text style={{ marginTop: 15, fontFamily: 'Inter-Bold', opacity: .5, fontSize: 17 }}>
+                                        Método pagamento
+                                </Text>
+                                    <Text style={{ paddingTop: 5, fontFamily: 'Inter-SemiBold', color: colors.GREEN.light, fontSize: 15 }}>
+                                        {payment.payment_mode}
+                                    </Text>
+                                </View>
+                                :
+                                <View style={{ alignSelf: 'center', flexDirection: 'column', alignItems: 'center' }}>
+                                    <Text style={{ marginTop: 15, fontFamily: 'Inter-Bold', opacity: .5, fontSize: 17 }}>
+                                        Método pagamento
+                                </Text>
+                                    <Text style={{ paddingTop: 5, fontFamily: 'Inter-SemiBold', color: colors.DEEP_SKY, fontSize: 15 }}>
+                                        {payment.payment_mode}
+                                    </Text>
+                                </View>
+
+                            : null
+                    }
+
+                    <View style={{ alignSelf: 'center' }}>
+                        {
+                            carType ?
+                                carType === COLT_ECONOMICO ?
+                                    <View style={{ flexDirection: 'row' }}>
+                                        <ColtEconomicoCar
+                                            width={120}
+                                            height={90}
+                                        />
+                                        <Text style={{ marginLeft: 10, fontFamily: 'Inter-Bold' }}>
+                                            {COLT_ECONOMICO}
+                                        </Text>
+                                    </View>
+                                    :
+                                    <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+                                        <ColtConfortCar
+                                            width={120}
+                                            height={90}
+                                        />
+                                        <Text style={{ marginLeft: 10, fontFamily: 'Inter-Bold' }}>
+                                            {COLT_CONFORT}
+                                        </Text>
+                                    </View>
+                                : null
+                        }
+                    </View>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        padding: 16,
+        height: height * 0.50,
+        elevation: 3,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: { x: 1, y: 1 },
+        shadowRadius: 10,
+    },
+    viewQueueBooking: {
+        backgroundColor: colors.transparent,
+        height: 45,
+        width: '100%',
+        alignSelf: 'center',
+        borderRadius: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: { x: 0, y: 5 },
+        shadowOpacity: 0.2,
+        shadowRadius: 10,
+        elevation: 3,
+    },
+})
+
+export default BottomSheetComponent;
--- Colt_code/ColtPassageiro_code/src/screens/BookedCabScreen.js
+++ Colt_code/ColtPassageiro_code/src/screens/BookedCabScreen.js
@@ -44,6 +44,8 @@ import LocationDrop from '../../assets/svg/LocationDrop';
 import LocationUser from '../../assets/svg/LocationUser';
 import LocationWaypoint from '../../assets/svg/LocationWaypoint';
 
+import BottomSheetComponent from '../components/BottomSheetComponent';
+
 const COLT_ECONOMICO = "Colt econômico"
 const COLT_CONFORT = "Colt confort"
 
@@ -83,7 +85,7 @@ export default class BookedCabScreen extends React.Component {
             driverLng: 0,
             driverAngle: 0
         }
-        this.driverFound = false
+        this.driverFound = true
         this.fadeAnim = new Animated.Value(0)
     }
 
@@ -93,7 +95,7 @@ export default class BookedCabScreen extends React.Component {
         this.state.bookingDataState == null ? this.getParamData = this.props.navigation.getParam('passData') : this.getParamData = this.state.bookingDataState
         let param = this.props.navigation.getParam('byMapScreen') ? this.props.navigation.getParam('byMapScreen') : null
         if (param == null) {
-            this.setState({ driverSearch: true, showBtnCancel: true })
+            this.setState({ driverSearch: false, showBtnCancel: true })
         }
 
         let curuser = firebase.auth().currentUser
@@ -168,7 +170,7 @@ export default class BookedCabScreen extends React.Component {
                 }
                 else if (currUserBooking.status == "CANCELLED") {
                     this.driverFound = false
-                    this.props.navigation.replace('FareDetails', { data: this.state.region, waypoint: this.state.waypoint ? this.state.waypoint : null  })
+                    this.props.navigation.replace('FareDetails', { data: this.state.region, waypoint: this.state.waypoint ? this.state.waypoint : null })
                 }
                 else if (currUserBooking.status == "TIMEOUT") {
                     this.driverFound = false
@@ -309,7 +311,7 @@ export default class BookedCabScreen extends React.Component {
                 this.setState({ driverSearch: false })
                 this.sendPushNotification(this.state.currentUser, "Estamos conectando você ao motorista.")
                 //this.fitDriverUser()
-                Linking.openURL("coltpassageiro://")
+                Linking.openURL('coltpassageiro://')
 
                 const driverRef = firebase.database().ref('users/' + this.driverObj.driverUid + '/')
                 driverRef.once('value', snap => {
@@ -736,115 +738,6 @@ export default class BookedCabScreen extends React.Component {
         }
     }
 
-    renderContent = () => (
-        <View
-            style={{
-                backgroundColor: 'white',
-                padding: 16,
-                height: height * 0.50,
-                elevation: 5,
-                shadowColor: '#000',
-                shadowOpacity: 0.2,
-                shadowOffset: { x: 1, y: 1 },
-                shadowRadius: 10,
-            }}
-        >
-            <TouchableOpacity style={{ width: 65, alignSelf: 'center', height: 6, backgroundColor: colors.GREY1, borderRadius: 50 }} />
-            <View style={styles.viewQueueBooking}>
-                <Wander
-                    size={40}
-                    color={colors.DEEPBLUE}
-                />
-                <Text style={{ textAlign: 'center', paddingHorizontal: 5, fontFamily: 'Inter-ExtraBold', fontSize: 15, color: colors.DEEPBLUE }}> Estamos conectando você ao motorista. </Text>
-            </View>
-            <View>
-                <View style={{ marginTop: 15 }}>
-
-                    <View style={{ marginTop: 10, flexDirection: 'row', alignItems: 'center' }}>
-                        <LocationUser
-                            width={20}
-                            height={20}
-                        />
-                        <Text style={{ marginLeft: 5 }}>{this.state.region ? this.state.region.whereText.split(',')[0] + this.state.region.whereText.split(',')[1] : null}</Text>
-                    </View>
-
-                    {this.state.waypoint ?
-                        <View>
-                            <View style={{ marginTop: 10, flexDirection: 'row', alignItems: 'center' }}>
-                                <LocationWaypoint
-                                    width={20}
-                                    height={20}
-                                />
-                                <Text style={{ marginLeft: 5 }}>{this.state.waypoint.add}</Text>
-                            </View>
-                        </View>
-                        :
-                        null}
-
-                    <View style={{ marginTop: 10, flexDirection: 'row', alignItems: 'center' }}>
-                        <LocationDrop
-                            width={22}
-                            height={22}
-                        />
-                        <Text style={{ marginLeft: 5 }}>{this.state.region ? this.state.region.droptext.split(',')[0] + this.state.region.droptext.split(',')[1] : null}</Text>
-                    </View>
-
-
-                    {
-                        this.state.payment ?
-                            this.state.payment.payment_mode === 'Dinheiro' ?
-                                <View style={{ alignSelf: 'center', flexDirection: 'column', alignItems: 'center' }}>
-                                    <Text style={{ marginTop: 15, fontFamily: 'Inter-Bold', opacity: .5, fontSize: 17 }}>
-                                        Método pagamento
-                                    </Text>
-                                    <Text style={{ paddingTop: 5, fontFamily: 'Inter-SemiBold', color: colors.GREEN.light, fontSize: 15 }}>
-                                        {this.state.payment.payment_mode}
-                                    </Text>
-                                </View>
-                                :
-                                <View style={{ alignSelf: 'center', flexDirection: 'column', alignItems: 'center' }}>
-                                    <Text style={{ marginTop: 15, fontFamily: 'Inter-Bold', opacity: .5, fontSize: 17 }}>
-                                        Método pagamento
-                                    </Text>
-                                    <Text style={{ paddingTop: 5, fontFamily: 'Inter-SemiBold', color: colors.DEEP_SKY, fontSize: 15 }}>
-                                        {this.state.payment.payment_mode}
-                                    </Text>
-                                </View>
-
-                            : null
-                    }
-
-                    <View style={{ alignSelf: 'center' }}>
-                        {
-                            this.state.carType ?
-                                this.state.carType === COLT_ECONOMICO ?
-                                    <View style={{ flexDirection: 'row' }}>
-                                        <ColtEconomicoCar
-                                            width={120}
-                                            height={90}
-                                        />
-                                        <Text style={{ marginLeft: 10, fontFamily: 'Inter-Bold' }}>
-                                            {COLT_ECONOMICO}
-                                        </Text>
-                                    </View>
-                                    :
-                                    <View style={{ flexDirection: 'column', alignItems: 'center' }}>
-                                        <ColtConfortCar
-                                            width={120}
-                                            height={90}
-                                        />
-                                        <Text style={{ marginLeft: 10, fontFamily: 'Inter-Bold' }}>
-                                            {COLT_CONFORT}
-                                        </Text>
-                                    </View>
-                                : null
-                        }
-                    </View>
-                </View>
-            </View>
-        </View>
-    )
-
     fadeIn() {
         Animated.timing(this.fadeAnim, {
             toValue: .3,
@@ -942,9 +835,17 @@ export default class BookedCabScreen extends React.Component {
                                     //onOpenEnd={() => console.log("TETSE")}
                                     onCloseEnd={() => this.fadeOut()}
                                     onOpenEnd={() => this.fadeIn()}
-                                    snapPoints={[100, '50%', 100]}
+                                    snapPoints={[100, '45%', 100]}
                                     borderRadius={20}
-                                    renderContent={this.renderContent}
+                                    renderContent={() =>
+                                        <BottomSheetComponent
+                                            customStyle={{ height: height * 0.45 }}
+                                            region={this.state.region ? this.state.region : null}
+                                            waypoint={this.state.waypoint ? this.state.waypoint : null}
+                                            payment={this.state.payment ? this.state.payment : null}
+                                            carType={this.state.carType ? this.state.carType : ""}
+                                        />
+                                    }
                                 />
 
                             </View>
@@ -953,8 +854,8 @@ export default class BookedCabScreen extends React.Component {
                                     interval={300}
                                     function={() => {
                                         console.log("CHAMOU AS FUNÇOES DO BACKGROUND")
-                                        this.selectNearbyDriver()
-                                        this.listenerStatus()
+                                        //this.selectNearbyDriver()
+                                        //this.listenerStatus()
                                     }}
                                 />
                             </View>
